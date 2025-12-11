322fdd7b4fafea4cab6259b4822bbe2951f54dea
abhishek <abhishek.km2010@gmail.com>
2025-12-11 13:49:25 +0530

    initial commit master

--- chat_models/chatmodel_openai.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = ChatOpenAI(model='gpt-4', max_completion_tokens=10) # Tokens = no. of words
-
-result = model.invoke('What is the capital of india ?')
-print(result.content)
